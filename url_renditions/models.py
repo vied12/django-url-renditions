57b0728bb68e85e26d0778babff6e668a0e721b2
Edouard Richard <vied12@gmail.com>
2018-11-06 14:30:53 +0100

    feat: django 2

--- url_renditions/models.py
+++ url_renditions/models.py
@@ -12,7 +12,7 @@ class Original(models.Model):
 
 
 class Rendition(models.Model):
-    original = models.ForeignKey(Original)
+    original = models.ForeignKey(Original, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     href = models.URLField()
     width = models.IntegerField(null=True, blank=True)
